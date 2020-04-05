ee78d33d1492e970c178d45ac3e0520c90d72c83
R2dmk <adamik.artur93@gmail.com>
2020-04-05 17:46:30 +0200

    first commit

---
+++ src/FirstContainer/FirstCont.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function FirstCont(){
+    return(
+        <div>
+            <video id="bg" src="FoodPack1_13_Videvo.mov"></video>
+            <p>aaa</p>
+        </div>
+    )
+}
+
+
+export default FirstCont;
---
+++ src/Header/Header.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+function Header(){
+    return(
+        <header>
+            <img src="https://scontent-frt3-2.xx.fbcdn.net/v/t1.0-9/38841223_1781441071941299_8577050265637617664_n.jpg?_nc_cat=103&_nc_sid=85a577&_nc_ohc=dDc3X3q-sa8AX_fDLpv&_nc_ht=scontent-frt3-2.xx&oh=b0546ca3bea3fe14b4570ced75e6d8e2&oe=5EA64C2B" alt="Power WEGE Box"/>
+            <p>VEGAN<br/>
+            Power<br/>
+            Box</p>
+            <p>Oferta</p>
+            <p>Menu</p>
+            <p>Cena</p>
+            <p>Kontakt</p>
+            <p>Gdzie dowozimy</p>
+            <img src="http://simpleicon.com/wp-content/uploads/Shopping-Cart-11.png" alt="Koszyk" className= "koszyk"/>
+        </header>
+    )
+}
+
+
+export default Header;
